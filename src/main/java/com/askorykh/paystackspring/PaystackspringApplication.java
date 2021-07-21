74d9efdbb0b7cd179e5fe4084c3db35f3a6299b3
Oleksii Skorykh <askorykh@gmail.com>
2021-07-21 18:05:46 +0100

    Initial commit

---
+++ src/main/java/com/askorykh/paystackspring/PaystackspringApplication.java
@@ -0,0 +1,15 @@
+package com.askorykh.paystackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaystackspringApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PaystackspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/askorykh/paystackspring/PaystackspringApplicationTests.java
@@ -0,0 +1,15 @@
+package com.askorykh.paystackspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaystackspringApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
